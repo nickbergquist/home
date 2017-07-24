fb46297119577f5e6b5cd7ab108e4cb8acdd62ed
Nick Bergquist <nick_bergquist@yahoo.co.uk>
2017-07-24 20:18:54 +0100

    Add new 'code' view

---
+++ Nab/Controllers/CodeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nab.Controllers
+{
+    public class CodeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
