2832341416aab556463c56f9e11c6dd49fde10a3
Nick Bergquist <nick_bergquist@yahoo.co.uk>
2017-08-02 01:08:54 +0100

    Enforce lowercase routing

--- Nab/Startup.cs
+++ Nab/Startup.cs
@@ -30,6 +30,8 @@ namespace Nab
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting(options => options.LowercaseUrls = true);
+            
             // Add framework services.
             services.AddMvc();
 
