1a529542f6cb945e6306ea0bb10f4e8f847ecd5a
Nick Bergquist <nick_bergquist@yahoo.co.uk>
2017-08-19 15:17:36 +0100

    Cleanup to project file and references after VS2017 update

--- Nab/wwwroot/_references.js
+++ Nab/wwwroot/_references.js
@@ -2,5 +2,4 @@
 /// <reference path="../gulpfile.js" />
 /// <reference path="../Assets/js/static/enquire.min.js" />
 /// <reference path="../Assets/js/static/matchmedia.js" />
-/// <reference path="../Assets/js/site.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
