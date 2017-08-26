9707e88afce634662cdbf7ff95f715280195d2fd
Nick Bergquist <nick_bergquist@yahoo.co.uk>
2017-08-26 11:54:02 +0100

    Add ascending/descending record ordering e2e tests

--- Nab/e2e-tests/scenarios.js
+++ Nab/e2e-tests/scenarios.js
@@ -28,14 +28,17 @@ describe('Record breakers component', () => {
 		it('should be possible to control record breakers order via the drop-down menu', () => {
 
 			var queryField = element(by.model('$ctrl.query'));
+
 			var orderSelect = element(by.model('$ctrl.orderProp'));
 			var nameOption = orderSelect.element(by.css('option[value="name"]'));
+			var recordDateDescOption = orderSelect.element(by.css('option[value="recordDate"]'));
+			var recordDateAscOption = orderSelect.element(by.css('option[value="-recordDate"]'));
 
 			// returns a promise resolving to an array containing all HTML elements with a binding to the record's name:
 			var recordBreakerNameColumn = element.all(by.repeater('record in $ctrl.records').column('record.name'));
 			
 			// use map array method - callback function taken as argument takes in the current array element
-			//and returns the new element to take its place in the new array:
+			// and returns the new element to take its place in the new array:
 			var getNames = () => recordBreakerNameColumn.map(elem => elem.getText());
 
 			//function getNames() {
@@ -43,20 +46,42 @@ describe('Record breakers component', () => {
 			//		return elem.getText();
 			//	});
 			//}
+			
+			// expect the default list using the orderBy filter on $ctrl.orderProp
+			expect(getNames()).toEqual([
+				'A1 No. 4472, "Flying Scotsman" - 1934',
+				'A3 No. 2750, "Papyrus" - 1935',
+				'A4 No. 2509, "Silver Link" - 1935',
+				'A4 No. 4468, "Mallard" - 1938'
+			]);
 
-			queryField.sendKeys('A4');   // narrow dataset to make the assertions shorter
+			recordDateAscOption.click();
 
 			expect(getNames()).toEqual([
-				'A4 No. 2509, "Silver Link"',
-				'A4 No. 4468, "Mallard"'
+				'A4 No. 4468, "Mallard" - 1938',
+				'A3 No. 2750, "Papyrus" - 1935',
+				'A4 No. 2509, "Silver Link" - 1935',
+				'A1 No. 4472, "Flying Scotsman" - 1934'
+			]);
+
+			// narrow dataset to make the assertions shorter
+			queryField.sendKeys('A4');
+
+			recordDateDescOption.click();
+
+			expect(getNames()).toEqual([
+				'A4 No. 2509, "Silver Link" - 1935',
+				'A4 No. 4468, "Mallard" - 1938'
 			]);
 
 			nameOption.click();
 
 			expect(getNames()).toEqual([
-				'A4 No. 4468, "Mallard"',
-				'A4 No. 2509, "Silver Link"'
+				'A4 No. 4468, "Mallard" - 1938',
+				'A4 No. 2509, "Silver Link" - 1935'
 			]);
+
+			
 		});
 
 	});
