f696776a2db6cff0d36a397291b0344e335be622
Nick Bergquist <nick_bergquist@yahoo.co.uk>
2019-01-09 17:14:53 +0000

    Remove obsolete logging API

--- Nab/Startup.cs
+++ Nab/Startup.cs
@@ -60,9 +60,6 @@ namespace Nab
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
             app.UseResponseCompression(); // won't work on an AWS S3 host without using Cloudflare CDN as request header "Accept-Encoding: gzip" is ignored
 
             if (env.IsDevelopment())
